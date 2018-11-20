05f2835bfa4e82bf99914617723531a65acc194a
Julius Bacosa <julius@innoventsoftware.com.au>
2018-11-20 20:17:05 +0800

    Add project files.

---
+++ SPECIFICATIONEXERCISE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPECIFICATIONEXERCISE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
